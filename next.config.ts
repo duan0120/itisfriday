8b8df8b9c607495487b2d60585502ecd6f25f7c9
duanhongjin <duanhjlt@gmail.com>
2025-04-17 23:07:24 +0800

    keep console

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // 只针对客户端代码
+      config.optimization.minimizer.forEach((minimizer: any) => {
+        if (minimizer.constructor.name === 'TerserPlugin') {
+          minimizer.options.terserOptions = {
+            ...minimizer.options.terserOptions,
+            compress: {
+              ...minimizer.options.terserOptions.compress,
+              drop_console: false, // 保留 console
+            },
+          };
+        }
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
